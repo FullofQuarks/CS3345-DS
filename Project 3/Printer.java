f7b059e3e0caf08f9b2895ca8e4c2072281a315f
FullofQuarks <nicholas.smith4@utdallas.edu>
2017-10-22 17:09:57 -0500

    Added ability to read a file
    Reads a file, and creates temporary variables to hold the values

--- Project 3/Printer.java
+++ Project 3/Printer.java
@@ -1,1 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Printer {
+
+	public static void main(String[] args) {
+		
+		String fileName = "input.txt";
+		String line = null;
+		String tempUserName = null;
+		int tempPriority = 0;
+		int tempNumPages = 0;
+		String tempIO = null;
+		
+		File file = new File(fileName);
+		try {
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextLine())
+			{
+				tempUserName = sc.next();
+				tempPriority = sc.nextInt();
+				tempNumPages = sc.nextInt();
+				tempIO = sc.next();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
