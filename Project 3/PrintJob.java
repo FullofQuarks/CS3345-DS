87804cf0e32f8f96c0ce6ee0b1d9cdc6e7d8aaa0
FullofQuarks <nicholas.smith4@utdallas.edu>
2017-10-22 17:37:56 -0500

    Update PrintJob.java

--- Project 3/PrintJob.java
+++ Project 3/PrintJob.java
@@ -1,1 +1,32 @@
-
+//import java.util.*;
+public class PrintJob {
+	private String userName;
+	protected int numPages;
+	private int priority;
+	
+	public PrintJob(String u, int num, int pri)
+	{
+		userName = u;
+		numPages = num;
+		priority = pri;
+	}
+	
+	public String getUserName()
+	{
+		return userName;
+	}
+	
+	public int getNumPages()
+	{
+		return numPages;
+	}
+	
+	public int getPriority()
+	{
+		return priority;
+	}
+	
+	public int compareTo(int arg0) {
+		return 0;
+	}
+}
