97da9b7590474d3748c2e8009fb56c968be8bbae
FullofQuarks <nicholas.smith4@utdallas.edu>
2017-10-22 17:38:21 -0500

    Update OutsidePrintJob.java

--- Project 3/OutsidePrintJob.java
+++ Project 3/OutsidePrintJob.java
@@ -1,1 +1,16 @@
 
+public class OutsidePrintJob extends PrintJob {
+
+	private float cost;
+	
+	public OutsidePrintJob(String u, int num, int pri) {
+		super(u, num, pri);
+		cost = (float) .1 * num;
+	}
+	
+	public float getCost()
+	{
+		return cost;
+	}
+
+}
