23320d856bbb6848d856024b362de8266f8cd155
FullofQuarks <nicholas.smith4@utdallas.edu>
2017-10-22 19:26:18 -0500

    Created ArrayList of PrintJobs

--- Project 3/Printer.java
+++ Project 3/Printer.java
@@ -1,20 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Printer {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		
 		String fileName = "input.txt";
-		String line = null;
 		String tempUserName = null;
 		int tempPriority = 0;
 		int tempNumPages = 0;
 		String tempIO = null;
+		List<PrintJob> jobs = new ArrayList<PrintJob>();
+		List<OutsidePrintJob> outJobs = new ArrayList<>();
+		int iJobs = 0, oJobs = 0;
 		
 		File file = new File(fileName);
-		try {
+		try { 
 			Scanner sc = new Scanner(file);
 			while(sc.hasNextLine())
 			{
@@ -22,12 +25,26 @@ public class Printer {
 				tempPriority = sc.nextInt();
 				tempNumPages = sc.nextInt();
 				tempIO = sc.next();
+				if(tempIO.equals("I"))
+				{
+					PrintJob newJob = new PrintJob(tempUserName, tempPriority, tempNumPages);
+					jobs.add(newJob);
+					iJobs++;
+				}
+				else
+				{
+					OutsidePrintJob newJob = new OutsidePrintJob(tempUserName, tempPriority, tempNumPages);
+					outJobs.add(newJob);
+					oJobs++;
+				}
 			}
 			
+			sc.close();
+			
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			System.out.println("Cannot find file named " + fileName + ".");
+			System.out.println("Please check spelling or file existence, and try again.");
 		}
-
 	}
 
 }
