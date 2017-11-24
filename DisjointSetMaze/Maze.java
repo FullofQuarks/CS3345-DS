f3e9bad050fadff1330eb5be277710370ebfcab0
Nicholas Smith <nicholas.smith4@utdallas.edu>
2017-11-24 14:58:06 -0600

    Create Maze.java

---
+++ DisjointSetMaze/Maze.java
@@ -0,0 +1,19 @@
+
+public class Maze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DisjSets cells = new DisjSets( 4 );
+		System.out.println(cells.find(3) == cells.find(0));
+		cells.union(0, 1);
+		cells.union(2, 3);
+		cells.union(0, 2);
+		System.out.println(cells.find(3) == cells.find(0));
+		//cells.union(0, 2);
+		cells.printAll();
+		//System.out.println(cells.find(0) == cells.find(3));
+		System.out.println();
+		System.out.println(cells.isOne());
+	}
+
+}
