a7a5dad2c6fced0a78d50744ef9f01c056276923
Nicholas Smith <nicholas.smith4@utdallas.edu>
2017-11-27 18:23:04 -0600

    Updated driver

--- DisjointSetMaze/MainMaze.java
+++ DisjointSetMaze/MainMaze.java
@@ -3,20 +3,9 @@ public class MainMaze {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DisjSets cells = new DisjSets( 4,4 );
-		System.out.println(cells.find(3) == cells.find(0));
-		cells.union(0, 1);
-		cells.union(2, 3);
-		cells.union(0, 2);
-		System.out.println(cells.find(3) == cells.find(0));
-		//cells.union(0, 2);
-		cells.printAll();
-		//System.out.println(cells.find(0) == cells.find(3));
-		System.out.println();
-		System.out.println(cells.isOne());
-		System.out.println();
+		DisjSets cells = new DisjSets( 1000,1000 );
+		System.out.println("Begin.");
 		cells.generateMaze();
-		cells.printAll();
 		System.out.println();
 		cells.printMaze();
 	}
